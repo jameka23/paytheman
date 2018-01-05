ded45b7c1044bf8a856a0b12f567865c00055d10
Jameka Echols <jameka.echols>
2018-01-05 17:17:04 -0600

    first commit

--- PayTheMan/ViewController.swift
+++ PayTheMan/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        print("Pay the man, ya got to pay the man. -Foster The People")
     }
 
     override func didReceiveMemoryWarning() {
